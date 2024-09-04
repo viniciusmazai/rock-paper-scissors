3ca5d683080dc0ef0e0f5485754b0c09b6fd010a
Vinicius Cesar <vinicius@cesar.etc.br>
2024-09-03 22:51:59 -0300

    add playRound() and playGame() functions

--- script.js
+++ script.js
@@ -8,3 +8,39 @@ function getHumanChoice() {
     return ["rock", "paper", "scissors"].includes(choice) ? choice : getHumanChoice();
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    const winConditions = {
+      rock: "scissors",
+      scissors: "paper",
+      paper: "rock"
+    };
+  
+    if (humanChoice === computerChoice) {
+      console.log("It's a tie!");
+    } else if (winConditions[humanChoice] === computerChoice) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore++;
+    }
+  }
+
+  function playGame() {
+    for (let i = 0; i < 5; i++) {
+      playRound(getHumanChoice(), getComputerChoice());
+      console.log(`Score: Human: ${humanScore}, Computer: ${computerScore}`);
+    }
+  
+    if (humanScore > computerScore) {
+      console.log("You won the game!");
+    } else if (computerScore > humanScore) {
+      console.log("Computer won the game!");
+    } else {
+      console.log("It's a tie game!");
+    }
+  }
+  
